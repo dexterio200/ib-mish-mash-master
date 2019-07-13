697198bd5e2c749c27a7f4cb3deae7dbbe821f02
Duc Trinh <ducanhtrinh93@gmail.com>
2019-07-13 17:13:32 +0200

    Update indentation

--- src/transform.js
+++ src/transform.js
@@ -1,4 +1,4 @@
-module.exports.groupAdultsByAgeRange = function(people) {
+module.exports.groupAdultsByAgeRange = function (people) {
     const categoriesByAge = {
         "20 and younger": [],
         "21-30": [],
@@ -10,30 +10,30 @@ module.exports.groupAdultsByAgeRange = function(people) {
     const categories = people
         .filter(person => person.age >= 18)
         .reduce((categories, person) => {
-        
-        if (person.age <= 20) {
-            categories["20 and younger"].push(person)
-            return categories
-        }
-        else if (21 <= person.age && person.age <= 30) {
-            categories["21-30"].push(person)
-            return categories
-        }
-        else if (31 <= person.age && person.age <= 40) {
-            categories["31-40"].push(person)
-            return categories
-        }
-        else if (41 <= person.age && person.age <= 50) {
-            categories["41-50"].push(person)
-            return categories
-        }
-        else {
-            categories["51 and older"].push(person)
-            return categories
-        }
-        
-    }, categoriesByAge)
-    
+
+            if (person.age <= 20) {
+                categories["20 and younger"].push(person)
+                return categories
+            }
+            else if (21 <= person.age && person.age <= 30) {
+                categories["21-30"].push(person)
+                return categories
+            }
+            else if (31 <= person.age && person.age <= 40) {
+                categories["31-40"].push(person)
+                return categories
+            }
+            else if (41 <= person.age && person.age <= 50) {
+                categories["41-50"].push(person)
+                return categories
+            }
+            else {
+                categories["51 and older"].push(person)
+                return categories
+            }
+
+        }, categoriesByAge)
+
     // Clean empty properties
     Object.keys(categories).forEach((key) => (categories[key].length === 0) && delete categories[key])
 
